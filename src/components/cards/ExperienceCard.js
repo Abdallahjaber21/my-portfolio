7ae6e3da41d05f76db3343662bfeb85051ab9d47
abdallah jaber <abdallah.j@codendot.com>
2023-10-25 13:01:32 +0300

    More Changes To The Exp Card

--- src/components/cards/ExperienceCard.js
+++ src/components/cards/ExperienceCard.js
@@ -10,7 +10,7 @@ const ExperienceCard = (props) => {
         <div className='experience_card flex w-144 mb-20'>
             <div className='from_to flex-1 w-10 text-sm text-muted text-capital'>{props?.from} - {props?.to}</div>
             <div className='details flex-auto w-64'>
-              <div className='title text-lg'>{props?.title}</div>
+              <div className='title text-md flex items-end gap-1'>{props?.title}</div>
               <div className='position text-sm text-muted'>{props?.position}</div>
               <div className='details mt-3 text-muted text-sm'>{props?.position_details}</div>
               <div className='skills mt-4 flex gap-2'>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import Right from '../components/home/Right';
 
 const Home = () => {
     return (
-         <div className="Home_page_layout bg-primary text-white">
+         <div className="Home_page_layout text-white  pb-32">
             <div className='content max-w-7xl mx-auto flex justify-between p-4'>
                 <Left />
                 <Right />
