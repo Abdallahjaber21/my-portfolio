c33593499e9028ca983384c77c2eb1dff02ad148
abdallah jaber <abdallah.j@codendot.com>
2023-10-25 14:38:49 +0300

    Added my cv and many more fixings

--- src/components/cards/ExperienceCard.js
+++ src/components/cards/ExperienceCard.js
@@ -7,13 +7,15 @@ const ExperienceCard = (props) => {
     console.warn('<<< skills >>>' , skills);
 
     return (
-        <div className='experience_card flex w-144 mb-20'>
-            <div className='from_to flex-1 w-10 text-sm text-muted text-capital'>{props?.from} - {props?.to}</div>
+        <div className='experience_card flex w-144 mb-10 p-2'>
+            <div className='from_to flex-1 w-10 text-sm text-muted text-capital font-semibold'>{props?.from} - {props?.to}</div>
             <div className='details flex-auto w-64'>
-              <div className='title text-md flex items-end gap-1'>{props?.title}</div>
-              <div className='position text-sm text-muted'>{props?.position}</div>
+              <div className='title text-md font-bold flex items-end gap-1'>
+                <a href={props?.company_url} target='_blank'>{props?.title}</a> 
+              </div>
+              <div className='position text-sm font-semibold text-muted'>{props?.position}</div>
               <div className='details mt-3 text-muted text-sm'>{props?.position_details}</div>
-              <div className='skills mt-4 flex gap-2'>
+              <div className='skills mt-4 flex gap-2 flex-wrap'>
                 {skills?.map((skill , index) => (
                     <div key={index} id={index} className='skill_tag rounded-full bg-teal-400/10 px-3 py-1 text-sm font-medium leading-5'>{skill}</div>
                  ))}
