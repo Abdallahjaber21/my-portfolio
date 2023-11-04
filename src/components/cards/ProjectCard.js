005fc52236c376d5df87051146567b04cbef8a83
abdallah jaber <abdallah.j@codendot.com>
2023-11-04 17:02:20 +0200

    lazy loading images

--- src/components/cards/ProjectCard.js
+++ src/components/cards/ProjectCard.js
@@ -9,7 +9,7 @@ const ProjectCard = (props) => {
       
         <div className='project_card scale_in flex 2xl:w-full xl:w-144 mb-10 p-2 gap-x-10 lg-break:w-full lg-break:p-0 xs:flex-col'>
         <div className='project_thumb_image overflow-hidden flex-1 2xl:w-14 xl:w-10 flex justify-center lg-break:justify-start items-start xs:w-full xs:justify-start'>
-          <img srcSet={props?.project_img} style={{background: props?.image_bg , padding: '5px'}} className={`${props?.image_scalable_class} object-contain rounded-sm max-h-36 max-w-56 w-fit object-left xs:h-fit transition-all duration-200`}/>
+          <img srcSet={props?.project_img} loading="lazy" style={{background: props?.image_bg , padding: '5px'}} className={`${props?.image_scalable_class} object-contain rounded-sm max-h-36 max-w-56 w-fit object-left xs:h-fit transition-all duration-200`}/>
         </div>
         <div className='details flex-auto w-64 xs:w-full xs:mt-2'>
           <div className='title text-md font-bold flex items-end gap-1'>
@@ -18,7 +18,7 @@ const ProjectCard = (props) => {
           <div className='details mt-3 text-sm flex gap-2 overflow-x-auto'>
             {/* {props?.project_details} */}
             {screenshots && screenshots?.map((img , index) => (
-                     <img key={index} id={index} srcSet={`/images/projects/`+img} alt="Image" className='h-[250px] w-[140px] xxs:w-full' />
+                     <img key={index} id={index} loading="lazy" srcSet={`/images/projects/`+img} alt="Image" className='h-[250px] w-[140px] xxs:w-full' />
             ))}
           </div>
           <div className='skills mt-4 flex gap-2 flex-wrap'>
